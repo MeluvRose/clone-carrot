5b18a63be7c8298759a59d296610b91142fe7016
meluvrose <tanggu3910@gmail.com>
2023-04-05 20:59:25 +0900

    #9.1 Accounts Logic

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -3,8 +3,89 @@ import client from "@libs/server/client";
 import withHandler from "@libs/server/withHandler";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
-  res.status(200).end();
+  const { phone, email } = req.body;
+  const payload = phone ? { phone: +phone } : { email };
+  // let user;
+
+  // upsert
+  /* 1.
+  if (email) {
+    user = await client.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (user) console.log("found it.");
+    else {
+      console.log("Did not found. Will create.");
+      user = await client.user.create({
+        data: {
+          name: "Anonymous",
+          email,
+        },
+      });
+    }
+    console.log(user);
+  }
+  if (phone) {
+    user = await client.user.findUnique({
+      where: {
+        phone: +phone,
+      },
+    });
+    if (user) console.log("found it.");
+    else {
+      console.log("Did not found. Will create.");
+      user = await client.user.create({
+        data: {
+          name: "Anonymous",
+          phone: +phone,
+        },
+      });
+    }
+    console.log(user);
+  }
+  */
+
+  /* 2.
+  if (phone) {
+    user = await client.user.upsert({
+      where: {
+        phone: +phone,
+      },
+      create: {
+        name: "Anonymous",
+        phone: +phone,
+      },
+      update: {},
+    });
+  } else if (email) {
+    user = await client.user.upsert({
+      where: {
+        email,
+      },
+      create: {
+        name: "Anonymous",
+        email,
+      },
+      update: {},
+    });
+  }
+  */
+  const user = await client.user.upsert({
+    where: {
+      // ...(phone ? { phone: +phone } : {}),
+      // ...(email ? { email } : {}),
+      ...payload,
+    },
+    create: {
+      name: "Anonymous",
+      ...payload,
+    },
+    update: {},
+  });
+  console.log(user);
+  return res.status(200).end();
 }
 
 export default withHandler("POST", handler);
