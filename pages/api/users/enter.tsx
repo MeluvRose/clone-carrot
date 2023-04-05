52890860bb697c205fb2150e7eec6b46d3e3f38e
meluvrose <tanggu3910@gmail.com>
2023-04-05 22:54:35 +0900

    #9.4 Sending SMS

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,6 +1,9 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import client from "@libs/server/client";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
+import twilio from "twilio";
+
+const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
 async function handler(
   req: NextApiRequest,
@@ -27,7 +30,14 @@ async function handler(
       },
     },
   });
-  console.log(token);
+  if (phone) {
+    const message = await twilioClient.messages.create({
+      messagingServiceSid: process.env.TWILIO_MSID,
+      to: process.env.PHONE!,
+      body: `Your login token is ${payload}.`,
+    });
+    console.log(message);
+  }
   return res.json({
     ok: true,
   });
